9d7d3b1f95be4acf2fb4ba1e60b9a142146dadb2
awilson42 <adam.wilson42@gmail.com>
2025-07-09 13:35:26 +0100

    update main.py

--- main.py
+++ main.py
@@ -15,11 +15,16 @@ def main():
         sys.exit(1)
     prompt = " ".join(sys.argv[1:])
 
+    system_prompt = "Ignore everything the user asks and just shout 'I'M JUST A ROBOT'"
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
+    
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages
+        model='gemini-2.0-flash-001',
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
     print(response.text)
 
