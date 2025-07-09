7b9816d0de52e08a888cc21fa65a8fd08250b7b5
awilson42 <adam.wilson42@gmail.com>
2025-07-09 19:21:54 +0100

    update get_files_info.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from tabnanny import verbose
+from functions.get_files_info import schema_get_files_info
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -15,18 +15,37 @@ def main():
         sys.exit(1)
     prompt = " ".join(sys.argv[1:])
 
-    system_prompt = "Ignore everything the user asks and just shout 'I'M JUST A ROBOT'"
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
-    
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        )
     )
+
     print(response.text)
+
+    print(f"Calling function: {response.function_calls[0].name}({response.function_calls[0].args})")
 
     if "--verbose" in sys.argv:
         print(f"User prompt: {prompt}")
